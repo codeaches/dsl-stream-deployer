daa0961b5129d4aa3831748f1b34ade506bd7dc2
info001-test <info001@codeaches.com>
2019-02-20 22:21:39 -0700

    no message

---
+++ src/main/java/com/codeaches/dslStreamDeployer/DemoApplication.java
@@ -0,0 +1,63 @@
+package com.codeaches.dslStreamDeployer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.dataflow.core.ApplicationType;
+import org.springframework.cloud.dataflow.rest.client.DataFlowOperations;
+import org.springframework.cloud.dataflow.rest.client.dsl.Stream;
+import org.springframework.cloud.dataflow.rest.resource.AppRegistrationResource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.PagedResources;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+
+			registerHttpSource();
+			registerLogSink();
+			createAndDeployStream();
+		};
+	}
+
+	@Autowired
+	DataFlowOperations dataFlowOperations;
+
+	void registerHttpSource() {
+
+		dataFlowOperations.appRegistryOperations().register("myHttpSource", ApplicationType.source,
+				"maven://org.springframework.cloud.stream.app:http-source-rabbit:2.1.0.RELEASE", null, true);
+	}
+
+	void registerLogSink() {
+
+		dataFlowOperations.appRegistryOperations().register("myLogSink", ApplicationType.sink,
+				"maven://org.springframework.cloud.stream.app:log-sink-rabbit:2.1.0.RELEASE", null, true);
+	}
+
+	void createAndDeployStream() {
+
+		Stream myStreamApp = Stream.builder(dataFlowOperations).name("myStreamApp")
+				.definition("myHttpSource | myLogSink").create().deploy();
+		System.out.println("Status --> " + myStreamApp.getStatus());
+	}
+
+	void destroyStream() {
+	}
+
+	void unregisterAllApps() {
+
+		PagedResources<AppRegistrationResource> registeredResources = dataFlowOperations.appRegistryOperations().list();
+		registeredResources.forEach(k -> {
+			dataFlowOperations.appRegistryOperations().unregister(k.getName(), ApplicationType.valueOf(k.getType()));
+		});
+	}
+}
